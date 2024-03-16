6dcb13e108d8eb0c0631541801553dc3df9542a3
K1NGAARON <gerardaaron1997@gmail.com>
2024-03-16 13:33:32 +0100

    copy and meta description

--- home/js/script.js
+++ home/js/script.js
@@ -1,16 +1,16 @@
 function changeReviews() {
     const reviews = [
         {
-            name: "Sidney",
-            review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem voluptates sunt rerum obcaecati. Quia quis quos voluptas id, saepe sint.",
+            name: "Ruben S.",
+            review: "Bij Resto mijn Plezier weten ze precies hoe ze hun gasten moeten verwennen. De ambiance is zo gezellig dat je je meteen thuisvoelt. En dan het eten... Hemels! Of je nu kiest voor een Thais gerecht of een klassieker van de kaart, je smaakpapillen worden gegarandeerd verwend. En dat allemaal voor een meer dan redelijke prijs. Dit is echt een pareltje onder de restaurants!",
         },
         {
-            name: "John P.",
-            review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem voluptates sunt rerum obcaecati. Quia quis quos voluptas id, saepe sint."
+            name: "Thomas D.",
+            review: "Een avondje bij Resto mijn Plezier voelt als thuiskomen. De gezelligheid spat ervan af zodra je binnenstapt. De sfeer is warm en uitnodigend, perfect voor een ontspannen diner met vrienden of familie. En het eten? Dat is simpelweg verrukkelijk! Elke hap is een smaakexplosie en de porties zijn royaal. En dan die prijs-kwaliteitverhouding, ongeëvenaard! Resto mijn Plezier is echt mijn favoriete plekje geworden."
         },
         {
-            name: "Will",
-            review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem voluptates sunt rerum obcaecati. Quia quis quos voluptas id, saepe sint.",
+            name: "Sofie V.",
+            review: "Een bezoekje aan Resto mijn Plezier is altijd een feestje. De gezelligheid, het lekkere eten en de vriendelijke bediening maken elke keer weer indruk op me. En de prijs-kwaliteitverhouding is gewoon top! Je krijgt hier echt waar voor je geld. Ik kan het iedereen aanraden die op zoek is naar een gezellige avond uit zonder de bank te breken.",
         },
     ];
 
